1a3b2ab44071e5109e5f9d387967f795d330d7bb
etiur <56678685+etiur@users.noreply.github.com>
2023-05-12 15:56:42 +0200

    fixed shap

--- feature_selection.py
+++ feature_selection.py
@@ -19,7 +19,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from multiprocessing import get_context # https://pythonspeed.com/articles/python-multiprocessing/
-shap.initjs(keep_js=False)
 
 
 def arg_parse():
