1bd6798da6b85320801fe8e40a0c6b543692ff8f
etiur <56678685+etiur@users.noreply.github.com>
2023-05-13 19:36:44 +0200

    remove the imports in init

--- BioML/__init__.py
+++ BioML/__init__.py
@@ -1,12 +1,1 @@
 __version__ = "1.0.0"
-
-from BioML.ensemble import *
-from BioML.feature_extraction import *
-from BioML.feature_selection import *
-from BioML.generate_model import *
-from BioML.generate_pssm import *
-from BioML.model_training import *
-from BioML.outlier import *
-from BioML.predict import *
-from BioML.threshold import *
-from BioML.utilities import *
