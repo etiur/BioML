7c5804b7f50725b34c30e421d6b1e6db2f30bd8b
etiur <56678685+etiur@users.noreply.github.com>
2023-05-13 19:27:17 +0200

    import the packages from the module

--- BioML/__init__.py
+++ BioML/__init__.py
@@ -1,12 +1,12 @@
 __version__ = "1.0.0"
 
-import ensemble
-import feature_extraction
-import feature_selection
-import generate_model
-import generate_pssm
-import model_training
-import outlier
-import predict
-import threshold
-import utilities
+import BioML.ensemble
+import BioML.feature_extraction
+import BioML.feature_selection
+import BioML.generate_model
+import BioML.generate_pssm
+import BioML.model_training
+import BioML.outlier
+import BioML.predict
+import BioML.threshold
+import BioML.utilities
