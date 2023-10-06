d28bf63d488ca2422a4736da8d8a7e3e9a1cd22b
ruite <ruite.xiang@gmail.com>
2023-10-06 06:55:12 +0200

    moved threshold to utilities

--- BioML/utilities.py
+++ BioML/utilities.py
@@ -11,6 +11,7 @@ from sklearn.neural_network import MLPClassifier
 import ast
 import numpy as np
 import logging
+from collections import Counter
 
 
 def scale(scaler, X_train, X_test=None):
@@ -229,3 +230,36 @@ class Log:
             Set to true to include the exception error message
         """
         self._logger.critical(messages, exc_info=exc_info)
+
+
+class Threshold:
+
+    def __init__(self, csv_file, output_csv, sep=","):
+        self.csv_file = pd.read_csv(csv_file, sep=sep, index_col=0)
+        self.output_csv = Path(output_csv)
+        self.output_csv.parent.mkdir(exist_ok=True, parents=True)
+
+    def apply_threshold(self, threshold, greater=True, column_name='temperature'):
+        """
+        Apply threshold to dataset
+        :param threshold: threshold value
+        :param greater: boolean value to indicate if threshold is greater or lower
+        :param column_name: column name to apply threshold
+        :return: filtered dataset
+        """
+        dataset = self.csv_file[column_name].copy()
+        dataset = pd.to_numeric(dataset, errors="coerce")
+        dataset.dropna(inplace=True)
+        data = dataset.copy()
+        if greater:
+            data.loc[dataset >= threshold] = 1
+            data.loc[dataset < threshold] = 0
+        else:
+            data.loc[dataset <= threshold] = 1
+            data.loc[dataset > threshold] = 0
+        print(f"using the threshold {threshold} returns these proportions", Counter(data))
+        return data
+
+    def save_csv(self, threshold, greater=True, column_name='temperature'):
+        data = self.apply_threshold(threshold, greater, column_name)
+        data.to_csv(self.output_csv)
