2e66eaee30c1516785963b65663f8ad76a50c163
ruite <ruite.xiang@gmail.com>
2023-10-09 18:21:10 +0200

    modified the test for model training

--- tests/context.py
+++ tests/context.py
@@ -4,3 +4,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import BioML.features as features
 from BioML.utilities import scale
+from BioML.model_training import Classifier, Regressor
--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -1,10 +1,14 @@
 import pytest
 import pandas as pd
 from sklearn.datasets import make_classification, make_regression
+from .context import scale, Classifier, Regressor
+from collections import defaultdict
+from pathlib import Path
+
 
 class TestRegressor:
     @pytest.fixture
-    def data_regression():
+    def data_re(self):
         X, Y = make_regression(n_samples=100, n_features=20, n_informative=10, noise=1, 
                                random_state=42)
         X_data = pd.DataFrame(X)
@@ -14,23 +18,22 @@ class TestRegressor:
         return X, Y, X_data, Y_data
     
     @pytest.fixture
-    def excel_regression():
-        X = pd.read_excel("../data/regression_selected.xlsx", index_col=0, header=[0,1])
-        return X, "../data/regression_selected.xlsx"
+    def trainer_re(self, data_re):
+        X, Y, X_data, Y_data = data_re
+        return Regressor(X_data, Y_data, "../data/training_output", seed=200)
 
-    def test_fix_feature_label(data_regression, excel_regression):
-        X, Y, X_data, Y_data = data_regression
-        X_excel, excel_file = excel_regression
-        
-
-
+    def test_fix_feature_label_re(self, data_re):
+        X, Y, X_data, Y_data = data_re
+        csv_file = Regressor(X, Y)
+        pandas = Regressor(X_data, Y_data)
+        excel_file = Regressor("../data/regression_selected.xlsx", Y)
 
 
 
 class TestClassifier:
     
     @pytest.fixture
-    def data_classification():
+    def data_cla():
         X, Y = make_classification(n_samples=100, n_features=20, n_informative=10, n_redundant=2, 
                                 n_repeated=0, n_classes=2)
         X_data = pd.DataFrame(X)
@@ -38,8 +41,14 @@ class TestClassifier:
         X_data.iloc[:,0] = 0
         Y_data.name = "target"
         return X, Y, X_data, Y_data
-    
+   
     @pytest.fixture
-    def excel_classification():
-        X = pd.read_excel("../data/classification_selected.xlsx", index_col=0, header=[0,1])
-        return X, "../data/classification_selected.xlsx"
+    def trainer_cla(self, data_cla):
+        X, Y = data_cla
+        return Classifier(X, Y, "../data/training_output", seed=200)
+    
+    def test_fix_feature_label(self, data_cla):
+        X, Y, X_data, Y_data = data_cla
+        csv_file = Classifier(X, Y)
+        pandas = Classifier(X_data, Y_data)
+        excel_file = Classifier("../data/classification_selected.xlsx", Y)
