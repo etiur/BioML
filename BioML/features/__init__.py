faa8600e86597dba5897d7471c687b15ef586b74
ruite <ruite.xiang@gmail.com>
2023-10-26 13:38:25 +0200

    modified initi ang git ignore

--- BioML/features/__init__.py
+++
@@ -1,3 +0,0 @@
-from .selection import FeatureClassification, FeatureRegression, FeatureSelection, DataReader
-from .extraction import ExtractFeatures, ReadFeatures
-from .generate_pssm import ExtractPssm
