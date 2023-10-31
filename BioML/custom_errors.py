326225dcfbbc6d53f6517ffe2889c0b49228f053
ruite <ruite.xiang@gmail.com>
2023-10-31 12:42:15 +0100

    added new custom error

--- BioML/custom_errors.py
+++ BioML/custom_errors.py
@@ -2,3 +2,8 @@ class NotSupportedDataError(Exception):
     """
     When the file provided is not supported yet
     """
+
+class DifferentLabelFeatureIndexError(Exception):
+    """
+    When the label and feature index are different
+    """
