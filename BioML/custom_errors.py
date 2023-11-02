a8fe5a40986e82c25dc79d67823a92159c0bc60e
ruite <ruite.xiang@gmail.com>
2023-11-02 12:58:44 +0100

    added a new error

--- BioML/custom_errors.py
+++ BioML/custom_errors.py
@@ -7,3 +7,8 @@ class DifferentLabelFeatureIndexError(Exception):
     """
     When the label and feature index are different
     """
+
+class SheetsNotFoundInExcelError(Exception):
+    """
+    When the sheets are not found in the excel file
+    """
