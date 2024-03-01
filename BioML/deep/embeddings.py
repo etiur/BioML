9b260e02019a69f766e479ab5cfeb35e0348da79
etiur <56678685+etiur@users.noreply.github.com>
2024-03-01 11:55:15 +0100

    added a format option to embeddings

--- BioML/deep/embeddings.py
+++ BioML/deep/embeddings.py
@@ -14,15 +14,19 @@ from ..utilities.utils import set_seed
 def arg_parse():
     parser = argparse.ArgumentParser(description="Generate embeddings from the protein large language model in Huggingface")
     parser.add_argument("fasta_file", type=str, help="Path to the FASTA file")
-    parser.add_argument("--model_name", type=str, default="facebook/esm2_t6_8M_UR50D", help="Name of the language model")
-    parser.add_argument("--disable_gpu", action="store_true", help="Whether to disable the GPU")
-    parser.add_argument("--batch_size", type=int, default=8, help="The batch size")
-    parser.add_argument("--save_path", type=str, default="embeddings.csv", help="The path to save the emebeddings in csv format")
-    parser.add_argument("--seed", type=int, default=12891245318, help="Seed for reproducibility")
-    parser.add_argument("--option", type=str, default="mean", help="Option to concatenate the embeddings")
+    parser.add_argument("-m", "--model_name", type=str, default="facebook/esm2_t6_8M_UR50D", 
+                        help="Name of the language model from huggingface")
+    parser.add_argument("-d", "--disable_gpu", action="store_true", help="Whether to disable the GPU")
+    parser.add_argument("-b", "--batch_size", type=int, default=8, help="The batch size")
+    parser.add_argument("-p", "--save_path", type=str, default="embeddings.csv", help="The path to save the emebeddings in csv format")
+    parser.add_argument("-s","--seed", type=int, default=12891245318, help="Seed for reproducibility")
+    parser.add_argument("-op", "--option", type=str, default="mean", help="Option to concatenate the embeddings")
+    parser.add_argument("-f", "--format", type=str, default="csv", choices=("csv", "parquet"), 
+                        help="Format to save the embeddings")
 
     args = parser.parse_args()
-    return [args.fasta_file, args.model_name, args.disable_gpu, args.batch_size, args.save_path, args.seed, args.option]
+    return [args.fasta_file, args.model_name, args.disable_gpu, args.batch_size, args.save_path, args.seed, args.option,
+            args.format]
 
 
 
@@ -157,9 +161,9 @@ class ExtractEmbeddings:
         """
         if option == "mean":
             return torch.mean(embedings, dim=0)
-        elif option == "sum":
+        if option == "sum":
             return torch.sum(embedings, dim=0)
-        elif option == "max":
+        if option == "max":
             return torch.max(embedings, dim=0)[0]
         else:
             raise ValueError("Option not available yet. Choose between 'mean', 'sum' or max")
@@ -193,7 +197,8 @@ class ExtractEmbeddings:
                                                                     option).detach().cpu().numpy()
         return results
 
-    def save(self, results: dict[str, np.array], path: str):
+    @staticmethod
+    def save(results: dict[str, np.array], path: str | Path):
         """
         Save the embeddings to a CSV file.
 
@@ -204,11 +209,28 @@ class ExtractEmbeddings:
         path : str
             Path to the CSV file.
         """
-        embeddings = pd.DataFrame(results).T
+        embeddings = pd.DataFrame(results).T 
         embeddings.to_csv(path, mode='a', header=not Path(path).exists())
     
+    @staticmethod
+    def convert_to_parquet(csv_file: str | Path, parquet_file: str | Path):
+        """
+        Convert a CSV file to parquet format.
+
+        Parameters
+        ----------
+        csv_file : str
+            Path to the CSV file.
+        parquet_file : str
+            Path to the parquet file.
+        """
+        df = pd.read_csv(csv_file, index_col=0)
+        df.to_parquet(parquet_file)
+        Path(csv_file).unlink()
+    
     def batch_extract_save(self, seq_keys: list[str], dataset: Dataset, batch_size: int=8, 
-                           save_path: str = "embeddings.csv", option: str = "mean"):
+                           save_path: str | Path = "embeddings.csv", option: str = "mean",
+                           format: str = "csv"):
         """
         Extract and save embeddings from a batch of sequences.
 
@@ -224,20 +246,25 @@ class ExtractEmbeddings:
             The path to save the emebeddings in csv format, by default "embeddings.csv"
         option : str, optional
             Option to concatenate the embeddings, by default "mean"
-        
+        format : str, optional
+            Format to save the embeddings, by default "csv" but can also be parquet
         Note
         ----
         This function saves at each batch iteration because it is thought for large files
         that doesn't fit in-memory.
         """
+        save_path = Path(save_path)
         for num, batch in enumerate(DataLoader(dataset, batch_size=batch_size)):
             batch_seq_keys = seq_keys[num*batch_size:(num+1)*batch_size]
             results = self.extract(batch_seq_keys, batch, option)
             self.save(results, save_path)
+        if format == "parquet":
+            self.convert_to_parquet(save_path, save_path.with_suffix(".parquet"))
 
 
 def generate_embeddings(model_name: str, fasta_file: str, disable_gpu: bool=False, 
-                        batch_size: int=8, save_path: str = "embeddings.csv", option: str = "mean"):
+                        batch_size: int=8, save_path: str = "embeddings.csv", 
+                        option: str = "mean", format: str = "csv"):
     """
     Generate embeddings from a FASTA file.
 
@@ -255,6 +282,8 @@ def generate_embeddings(model_name: str, fasta_file: str, disable_gpu: bool=Fals
         The path to save the emebeddings in csv format, by default "embeddings.csv"
     option : str, optional
         Option to concatenate the embeddings, by default "mean"
+    format : str, optional
+        Format to save the embeddings, by default "csv" but can also be parquet
     """
 
     config = LLMConfig(model_name, disable_gpu=disable_gpu)
@@ -263,12 +292,13 @@ def generate_embeddings(model_name: str, fasta_file: str, disable_gpu: bool=Fals
     tok = tokenizer.tokenize(fasta_file)
 
     # even if I have more columns in tok, it will only get the input_ids and the attention_mask
-    embeddings.batch_extract_save(tok["id"], tok, batch_size, save_path, option)
+    embeddings.batch_extract_save(tok["id"], tok, batch_size, save_path, option, format)
+
 
 def main():
-    fasta_file, model_name, disable_gpu, batch_size, save_path, seed, option = arg_parse()
+    fasta_file, model_name, disable_gpu, batch_size, save_path, seed, option, format = arg_parse()
     set_seed(seed)
-    generate_embeddings(model_name, fasta_file, disable_gpu, batch_size, save_path, option)
+    generate_embeddings(model_name, fasta_file, disable_gpu, batch_size, save_path, option, format)
 
 
 if __name__ == "__main__":
